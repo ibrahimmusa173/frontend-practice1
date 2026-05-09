c1bd7af710b3b5cfe38d5a469de8d6bf8d2dba20
Musa Aman <musaaman705@gmail.com>
2026-05-09 18:30:01 +0500

    name change

--- my-app/src/features/userSlice.js
+++ my-app/src/features/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
   name: 'user',
   initialState: {
-    userName: "Kiran"
+    userName: "ibrahim"
   },
   reducers: {
     // Function to update the name
